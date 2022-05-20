7f2698ec7137aae57db96ddc74ae52608a302989
panjianping <panjianping@sinokorealife.com.cn>
2022-05-20 09:58:37 +0800

    pdf转base64

--- inkwy-database/src/main/java/com/inkwy/database/controller/HealthController.java
+++ inkwy-database/src/main/java/com/inkwy/database/controller/HealthController.java
@@ -1,15 +1,23 @@
 package com.inkwy.database.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
-
+/**
+ * @description: 健康检查
+ * @author: pjp
+ * @date: 2022/5/20 9:28
+ */
 @RestController
+@Api(tags = "健康检查", value = "HealthController")
 public class HealthController {
 
     @GetMapping("/health")
+    @ApiOperation("健康检查")
     public Map<String, String> health() {
         Map<String, String> map = new HashMap<>(1);
         map.put("status", "UP");
--- inkwy-database/src/main/java/com/inkwy/database/controller/TestController.java
+++ inkwy-database/src/main/java/com/inkwy/database/controller/TestController.java
@@ -1,18 +1,19 @@
 package com.inkwy.database.controller;
 
+import cn.hutool.core.io.FileUtil;
 import com.inkwy.database.domain.Response;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.*;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
+import java.util.Base64;
 import java.util.Enumeration;
 
 /**
@@ -37,6 +38,63 @@ public class TestController {
     @ApiOperation("接口测试")
     public Response init(){
         return Response.respSuccess("success");
+    }
+
+    @PostMapping("/pdfToBase64")
+    @ApiOperation("pdf转Base64")
+    public String  pdfToBase64(MultipartFile multipartFile) {
+        Base64.Encoder encoder = Base64.getEncoder();
+        FileInputStream fin =null;
+        BufferedInputStream bin =null;
+        ByteArrayOutputStream baos = null;
+        BufferedOutputStream bout =null;
+        File file = null;
+        try {
+            file = transferToFile(multipartFile);
+            fin = new FileInputStream(file);
+            bin = new BufferedInputStream(fin);
+            baos = new ByteArrayOutputStream();
+            bout = new BufferedOutputStream(baos);
+            byte[] buffer = new byte[1024];
+            int len = bin.read(buffer);
+            while(len != -1){
+                bout.write(buffer, 0, len);
+                len = bin.read(buffer);
+            }
+            //刷新此输出流并强制写出所有缓冲的输出字节
+            bout.flush();
+            byte[] bytes = baos.toByteArray();
+            log.info("--------------------------");
+            System.out.println(encoder.encodeToString(bytes));
+            return encoder.encodeToString(bytes);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally{
+            try {
+                fin.close();
+                bin.close();
+                bout.close();
+                FileUtil.del(file);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    public File transferToFile(MultipartFile multipartFile) {
+        //选择用缓冲区来实现这个转换即使用java 创建的临时文件 使用 MultipartFile.transferto()方法 。
+        File file = null;
+        try {
+            file = new File("D:\\usr\\test.pdf");
+            multipartFile.transferTo(file);
+            file.deleteOnExit();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return file;
     }
 
     public static String getInetAddresses() {
